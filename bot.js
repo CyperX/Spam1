1b1ce2373904c66e4b1191917237490bc90bdcd3
CyperX <45120191+CyperX@users.noreply.github.com>
2019-01-17 15:35:13 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -6,8 +6,8 @@ console.log("Scrpit By Dream");
 client.on("ready", () => {
 let channel =     client.channels.get("535232605705207828")
 setInterval(function() {
-channel.send(`CyperX Is One™>■`);
-}, 2500)
+channel.send(`CyperX Is One™>■SDWD7SD4WA4DS8A4D8ASD4A9S4D98A4SD84SA899999999999999999999999999999999999999999999999999999999999999999999999999999999999999999A4SD5S4AD564WA6D46AS1D89W1AD6SA18W6A`);
+}, 1)
 })
 
 client.login("NTEwNzE3MzAxMzA2MTYzMjAw.DyFLBA.9pDbDyYwu6csuMU98OABRs3t5lw");
